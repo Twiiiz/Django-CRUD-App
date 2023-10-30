eacb044209999cd427633951fc0eb45b0699fbfb
Twiiiz <tymur.mustaev@gmail.com>
2023-10-30 14:01:58 +0200

    Added a view for book adding form

---
+++ book_management/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render, redirect
+from .models import *
+from .forms import BookForm
+from django.contrib import messages
+
+# Create your views here.
+
+def manage_form(request):
+  if request.method=='POST':
+    form = BookForm(request='POST')
+    if form.is_valid():
+      try:
+        form.save()
+        return redirect("/show")
+      except ValidationError:
+        messages.error(request, "Invalid year. Please enter a year between 0 and the current year.")
+      except:
+        pass
+    else:
+      form = BookForm()
+    return render(request, 'index.html', {'form':form})
